dfe93d05f0205015721a6ff05e318ffa075008e1
luan.nguyen <luanga316@gmail.com>
2024-01-09 23:37:18 +0700

    registerForm_UI  use  React-hook,query

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,7 @@ import {
   Navigate,
 } from "react-router-dom";
 import Layout from "./layouts/Layout";
+import Register from "./pages/Register";
 
 const App = () => {
   return (
@@ -22,6 +23,11 @@ const App = () => {
             </div>
           </Layout>}
         />
+        <Route path="/register" element={
+          <Layout>
+            <Register />
+          </Layout>
+        } />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Router>
